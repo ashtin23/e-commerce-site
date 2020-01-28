d108038fda3a96030ad6567ff7c26cb37c4a8ca0
ashtinlatting <rlatting92@gwu.edu>
2020-01-27 21:56:55 -0500

    firestore database created, as well as snapShot and other relevant data inside of App.js and firebase.utils

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -13,6 +13,34 @@ const config = {
     measurementId: "G-P0WLLR8HEL"
   };
 
+  export const createUserProfileDocument = async (userAuth, additionalData) => {
+    //if userAuth = null, we want to exit from this function
+    if (!userAuth) return;
+
+    const userRef = firestore.doc(`users/${userAuth.uid}`);
+
+    const snapShot = await userRef.get();
+
+   if(!snapShot.exists) {
+     const { displayName, email } = userAuth;
+     const createdAt = new Date();
+
+     //if there isn't any data, we are creating a new one using userAuth document
+     try {
+       await userRef.set({
+         displayName,
+         email,
+         createdAt,
+         ...additionalData
+       })
+     } catch(error) {
+        console.log('error creating user', error.message);
+     }
+   }
+
+   return userRef;
+  };
+
   firebase.initializeApp(config);
 
   export const auth = firebase.auth();
