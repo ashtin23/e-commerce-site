f7026c57f4183aa5272ea9548c640231670d0bc2
ashtinlatting <rlatting92@gwu.edu>
2020-02-02 11:46:23 -0500

    added additem and removeitem functionality to checkout item

--- src/redux/cart/cart.reducer.js
+++ src/redux/cart/cart.reducer.js
@@ -1,5 +1,7 @@
 import CartActionTypes from './cart.types';
-import { addItemToCart } from './cart.utils';
+import { addItemToCart, removeItemFromCart } from './cart.utils';
+import { clearItemFromCart } from './cart.actions';
+import CartItem from '../../components/cart-item/cart-item.component';
 
 const INITIAL_STATE = {
     hidden: true,
@@ -13,10 +15,24 @@ const cartReducer = (state = INITIAL_STATE, action) => {
                 ...state, 
                 hidden: !state.hidden
             };
-            case CartActionTypes.ADD_ITEM:
+        case CartActionTypes.ADD_ITEM:
             return {
                 ...state,
                 cartItems: addItemToCart(state.cartItems, action.payload)
+            };
+        case CartActionTypes.REMOVE_ITEM:
+            return {
+                ...state,
+                cartItems: removeItemFromCart(state.cartItems, action.payload)
+            };    
+        case CartActionTypes.CLEAR_ITEM_FROM_CART:
+            return {
+                 ...state,
+                 //if the cartItem.id does not match action.payload.id, return true
+                 //and we will keep it
+                cartItems: state.cartItems.filter(
+                    cartItem => cartItem.id !== action.payload.id
+                )
             };
             default: 
                 return state;
--- src/redux/cart/cart.utils.js
+++ src/redux/cart/cart.utils.js
@@ -1,15 +1,32 @@
 export const addItemToCart = (cartItems, cartItemToAdd) => {
-    const existingCartItem = cartItems.find(
-        cartItem => cartItem.id === cartItemToAdd.id 
-    );
+  const existingCartItem = cartItems.find(
+    cartItem => cartItem.id === cartItemToAdd.id
+  );
 
-    if(existingCartItem) {
-        return cartItems.map(cartItem => 
-            cartItem.id === cartItemToAdd.id
-            ? { ...cartItem, quantity: cartItem.quantity + 1 }
-            : cartItem
-            );
-    }
-    //quantity prop gets attached the 1st time around since the if block won't run when it's a new item
-    return [...cartItems, { ...cartItemToAdd, quantity: 1 }]
-} 
+  if (existingCartItem) {
+    return cartItems.map(cartItem =>
+      cartItem.id === cartItemToAdd.id
+        ? { ...cartItem, quantity: cartItem.quantity + 1 }
+        : cartItem
+    );
+  }
+  //quantity prop gets attached the 1st time around since the if block won't run when it's a new item
+  return [...cartItems, { ...cartItemToAdd, quantity: 1 }];
+};
+
+export const removeItemFromCart = (cartItems, cartItemToRemove) => {
+  const existingCartItem = cartItems.find(
+    cartItem => cartItem.id === cartItemToRemove.id
+  );
+
+  if (existingCartItem.quantity === 1) {
+      //returning any cart items that do not match so they are still visible in the cart
+    return cartItems.filter(cartItem => cartItem.id !== cartItemToRemove.id);
+  }
+
+  return cartItems.map(cartItem =>
+    cartItem.id === cartItemToRemove.id
+      ? { ...cartItem, quantity: cartItem.quantity - 1 }
+      : cartItem
+  );
+};
